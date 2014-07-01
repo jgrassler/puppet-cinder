2ac6c55783bd59e1ac2f90058a77431b6f69b410
Johannes Grassler <j.grassler@syseleven.de>
2014-07-01 17:16:34 +0200

    Added log format string configuration options.

--- manifests/init.pp
+++ manifests/init.pp
@@ -47,6 +47,22 @@
 #   (optional) Directory where logs should be stored.
 #   If set to boolean false, it will not log to any directory.
 #   Defaults to '/var/log/cinder'
+# 
+# [*logging_context_format_string*]
+#   (optional) Format string to use for log messages with context.
+#   Defaults to undef
+#
+# [*logging_default_format_string*]
+#   (optional) Format string to use for log messages without context.
+#   Defaults to undef
+#
+# [*logging_debug_format_suffix*]
+#   (optional) Data to append to log format when level is DEBUG.
+#   Defaults to undef
+#
+# [*logging_exception_prefix*]
+#   (optional) Prefix each line of exception output with this format.
+#   Defaults to undef
 #
 # [*use_ssl*]
 #   (optional) Enable SSL on the API server
@@ -124,6 +140,10 @@ class cinder (
   $use_syslog                  = false,
   $log_facility                = 'LOG_USER',
   $log_dir                     = '/var/log/cinder',
+  $logging_context_format_string = undef,
+  $logging_default_format_string = undef,
+  $logging_debug_format_suffix = undef,
+  $logging_exception_prefix = undef,
   $verbose                     = false,
   $debug                       = false,
   $mysql_module                = '0.9',
@@ -322,6 +342,33 @@ class cinder (
       'DEFAULT/log_dir': ensure => absent;
     }
   }
+
+  # Log format
+
+  if $logging_context_format_string {
+    cinder_config {
+      'DEFAULT/logging_context_format_string' : value => $logging_context_format_string;
+      }
+    }
+
+  if $logging_default_format_string {
+    cinder_config {
+      'DEFAULT/logging_default_format_string' : value => $logging_default_format_string;
+      }
+    }
+
+  if $logging_debug_format_suffix {
+    cinder_config {
+      'DEFAULT/logging_debug_format_suffix' : value => $logging_debug_format_suffix;
+      }
+    }
+
+  if $logging_exception_prefix {
+    cinder_config {
+      'DEFAULT/logging_exception_prefix' : value => $logging_exception_prefix;
+      }
+    }
+
 
   # SSL Options
   if $use_ssl {
